7374b0cd96fb48c2d4c81a1b8843c2f59d1cf57f
Jose Borunda <jose.h.borunda@gmail.com>
2019-09-23 18:27:27 -0600

    creatred server.js, created db files, and started connection with db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Chorizo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mexican Burger');
+INSERT INTO burgers (burger_name) VALUES ('Wagyu Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
