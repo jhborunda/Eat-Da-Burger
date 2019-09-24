e43c2c89c8aa44509a548b2d2ec69ceedcbcd6ab
Jose Borunda <jose.h.borunda@gmail.com>
2019-09-23 19:38:02 -0600

    revised burger.js

--- models/burger.js
+++ models/burger.js
@@ -8,22 +8,22 @@ var burger = {
     });
   },
   // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+  create: function(vals, cb) {
+    orm.create("burgers", ["burger_name"], vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = cat;
+module.exports = burger;
