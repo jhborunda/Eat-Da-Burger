5439842fdd56bb8cab5fe32c7355b830e8547039
Jose Borunda <jose.h.borunda@gmail.com>
2019-09-23 19:32:49 -0600

    used cats model for reference

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 var express = require("express");
-
+var bodyParser = require('body-parser');
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// // Parse application body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// Parse application body as JSON
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-// // Set Handlebars.
-// var exphbs = require("express-handlebars");
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/burgers_controller.js");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-// app.use(routes);
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
